68bb673c7064d72486226796de7d7c17c17ced1a
Hyaguinhogp <hyago.v.a.m@gmail.com>
2023-01-03 08:34:18 -0400

    fix: request user

--- ongs/views.py
+++ ongs/views.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Ong
 from .serializers import OngSerializer
@@ -9,7 +10,7 @@ from .permissions import IsAdmOrCreateOnly
 
 class OngView(ListCreateAPIView, PageNumberPagination):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAdmOrCreateOnly]
+    permission_classes = [IsAuthenticated, IsAdmOrCreateOnly]
 
     queryset = Ong.objects.all()
     serializer_class = OngSerializer
