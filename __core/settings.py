b31438aa80b9f4bbeb7e1bb40f0bda3ceb76b7c5
unknown <elizeucosta21@gmail.com>
2023-01-03 08:28:51 -0300

    refactor: sqlite as default database

--- __core/settings.py
+++ __core/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = "__core.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
+    "postgres": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
@@ -97,7 +97,7 @@ DATABASES = {
         "HOST": "127.0.0.1",
         "PORT": 5432,
     },
-    "sqlite3": {
+    "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
